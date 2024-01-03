d255e77a1d741b62d50a6035d66ca762afc20b3b
Avaya894 <bajracharyaavaya52@gmai.com>
2024-01-04 00:31:58 +0545

    re-fix outDir

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  outDir: 'dist',
+  outDir: '/bin/staticsites/51cbfc71-21e4-4baf-b61a-0ef741c1d8c3-swa-oryx/app',
   build: {
     rollupOptions: {
       external: [
